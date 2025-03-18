595e21ba1ca776601767b15d3ed94ed5df58f40a
RuslanGon <ruslan441006@gmail.com>
2025-03-18 12:23:08 +0200

    wer

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose';
+
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('hello word')
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is run on port ${PORT}`);
-})
+async function startServer() {
+    try {
+      await mongoose.connect('mongodb+srv://book:book@cluster0.9yeu1fb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+      app.listen(PORT, () => {
+        console.log(`DB is ok, Server start port on ${PORT}`);
+      });
+    } catch (error) {
+      console.log("DB is error");
+    }
+  }
+  startServer();
